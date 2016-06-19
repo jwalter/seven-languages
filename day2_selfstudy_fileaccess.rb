d9b8d4051791cd18d7faaa229f5e74ca1ad936f5
joakimwaltersson <joakim.waltersson@gmail.com>
2016-06-19 16:31:06 +0000

    Moved Ruby stuff to separate folder

--- day2_selfstudy_fileaccess.rb
+++
@@ -1,5 +0,0 @@
-File.open('README.md', 'r') {|f| puts f.read }
-
-f = File.open('README.md')
-puts f.read
-f.close
--- day2_selfstudy_hashtoarray.rb
+++
@@ -1,4 +0,0 @@
-h = { :a => 'Aa', :b => 'Bb' }
-a = []
-h.each {|key, value| a.push "#{key}:#{value}" }
-puts a
